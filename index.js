de617811424cd1571485bdfbe6f8933b35a843ef
feriteja <feriteja@gmail.com>
2022-07-06 10:13:44 +0700

    fix introduction

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const birthDate = "25/09/1996";
 const address = "Bandung";
 
 console.log(`Hello my name is ${name}`);
-console.log(`my birthday is about two month away, it's ${birthDate} :)`);
+console.log(`my birthday is  ${birthDate} `);
 console.log(`i live in ${address}`);
