920c82241e64bb8d9e92732366b9e3b1ae8e9528
feriteja <feriteja@gmail.com>
2022-07-06 10:11:24 +0700

    fix introduction

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const name = "Feri Teja Kusuma";
+const birthDate = "25/09/1996";
+const address = "Bandung";
 
-console.log(name);
+console.log(`Hello my name is ${name}`);
+console.log(`my birthday is about two month away, it's ${birthDate} :)`);
+console.log(`i live in ${address}`);
